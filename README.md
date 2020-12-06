d677ba55cd10842ab0a222e00db0807ff0779765
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2020-12-06 11:40:08 -0600

    readme edits

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ In order to run your application, type:
   $ cd .
   $ flutter run
 ```
-Your application code is in .\lib\main.dart.  
+The application code is in .\lib\main.dart.  
+
+How to get Google Firebase API Key  
+inside of ```android >> app```, add a ```google-services.json``` with the API keys to use Firebase.  
+To generate this file, make a Firebase app through Firebase and activate the ImageTextRecognition API.  
 
 Run in terminal with
 ```$ flutter emulators``` to check your available emulators  
@@ -19,15 +23,21 @@ Run in terminal with
 ## How Our App Works
 Doctors turn on Scriber when surveying patients for FHIR medical record data. Instead of having to multitask and record while listening to their patient, doctors can focus solely on the patient and let Scriber record the information and fill in the records. This saves time and manpower in the medical field. 
 
-## Technology Used
+## Technologies Used
 - Flutter
-- Google OCR
+- Google OCR (Firebase)
+- Firebase
 - Speech-to-text library from [csdcorp](https://pub.dev/packages/speech_to_text) installed through [pub.dev](https://flutter.dev/docs/development/packages-and-plugins/using-packages)
 - FHIR API
 
+## To-do
+- Edit/delete records
+- Convert to FHIR
+- Enable login
+
 ## Contributions
-Akhil: Firebase  
-Ivy: Login  
-Jeff:  
-Luke: Speech  
-Thao: Speech  
+Akhil: Firebase, image, transitions, textio, viewRecords  
+Ivy: Login, mainpage  
+Jeff:   
+Luke: Speech, speech JSON store, transitions  
+Thao: Speech, Trello  
---
+++ lib/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;       // page number
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: 'My Records',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.remove_red_eye),
+            label: 'Scan in Report',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mic),
+            label: 'Listen in',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Scriber'),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
