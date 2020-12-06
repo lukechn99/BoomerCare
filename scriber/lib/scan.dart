ee203baf83af0687fd4395c7f95090bc22efb4e2
aktalasi <aktalasi@ncsu.edu>
2020-12-06 17:01:58 +0530

    Document to text works and saves data locally

--- scriber/lib/scan.dart
+++ scriber/lib/scan.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:scriber/pickImage.dart';
 
+const String kFileName = 'data.json';
+
 class ScanScreen extends StatefulWidget {
+  String title = "Scan in a record";
+
   @override
   _ScanScreenState createState() => _ScanScreenState();
 }
@@ -13,10 +19,93 @@ class _ScanScreenState extends State<ScanScreen> {
 
   var result = "";
   File _userImageFile;
+  int _selectedIndex = 1;
+
+  TextEditingController _controllerKey, _controllerValue;
+
+  File _filePath;
+  bool _fileExists;
+
+  Map<String, dynamic> _json = {"documents":[]};
+  String _jsonString;
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/$kFileName');
+  }
+
+  @override
+  void initState()  {
+    super.initState();
+    // Instantiate _controllerKey and _controllerValue
+    _controllerKey = TextEditingController();
+    _controllerValue = TextEditingController();
+    print('0. Initialized _json: $_json');
+    _readJson();
+  }
+
+  void _readJson() async {
+    // Initialize _filePath
+    _filePath = await _localFile;
+
+    // 0. Check whether the _file exists
+    _fileExists = await _filePath.exists();
+    print('0. File exists? $_fileExists');
+
+    // If the _file exists->read it: update initialized _json by what's in the _file
+    if (_fileExists) {
+      try {
+        //1. Read _jsonString<String> from the _file.
+        _jsonString = await _filePath.readAsString();
+        print('1.(_readJson) _jsonString: $_jsonString');
+
+        //2. Update initialized _json by converting _jsonString<String>->_json<Map>
+        _json = jsonDecode(_jsonString);
+        print('2.(_readJson) _json: $_json \n - \n');
+      } catch (e) {
+        // Print exception errors
+        print('Tried reading _file error: $e');
+        // If encountering an error, return null
+      }
+    } else {
+      _writeJson("documents", []);
+    }
+  }
+
+  void _addToList(Map<String, dynamic> newDoc) async {
+    _json["documents"].add(newDoc);
+    _jsonString = jsonEncode(_json);
+    _filePath.writeAsString(_jsonString);
+  }
+
+  void _writeJson(String key, dynamic value) async {
+    // Initialize the local _filePath
+    //final _filePath = await _localFile;
+
+    //1. Create _newJson<Map> from input<TextField>
+    Map<String, dynamic> _newJson = {key: value};
+    print('1.(_writeJson) _newJson: $_newJson');
+
+    //2. Update _json by adding _newJson<Map> -> _json<Map>
+    _json.addAll(_newJson);
+    print('2.(_writeJson) _json(updated): $_json');
+
+    //3. Convert _json ->_jsonString
+    _jsonString = jsonEncode(_json);
+    print('3.(_writeJson) _jsonString: $_jsonString\n - \n');
+
+    //4. Write _jsonString to the _filePath
+    _filePath.writeAsString(_jsonString);
+  }
 
   recogniseText() async {
     FirebaseVisionImage myImage = FirebaseVisionImage.fromFile(_userImageFile);
-    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    TextRecognizer recognizeText = FirebaseVision.instance.cloudTextRecognizer();
     VisionText readText = await recognizeText.processImage(myImage);
     result = "";
     for (TextBlock block in readText.blocks) {
@@ -26,11 +115,19 @@ class _ScanScreenState extends State<ScanScreen> {
         });
       }
     }
+    Map<String, dynamic> document = {"title" : readText.blocks[0].text, "content": result};
+    _addToList(document);
   }
 
   void _pickedImage(File image) {
     _userImageFile = image;
     recogniseText();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -43,13 +140,34 @@ class _ScanScreenState extends State<ScanScreen> {
                 UserImagePicker(_pickedImage),
                 Padding(
                     padding: const EdgeInsets.all(16.0), child: Text(result)),
-                Text(
-              'The text in the image is: $result',
-            ),
               ],
             ),
           ),
-        )
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.article),
+          label: 'My Records',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.remove_red_eye),
+          label: 'Scan in Report',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.mic),
+          label: 'Listen in',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+    ),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      )
       );
   }
   
